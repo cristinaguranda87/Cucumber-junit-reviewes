339dc56829ff97aa1df6ee6c2b6b2e4e06b67b13
Cristina_Guranda <cbalanuta@gmail.com>
2023-02-18 15:55:56 -0500

    scenario outline is added

--- src/test/java/com/cydeo/step_definitions/LoginSeamlessly_stepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/LoginSeamlessly_stepDefinitions.java
@@ -10,31 +10,55 @@ import org.junit.Assert;
 
 public class LoginSeamlessly_stepDefinitions {
 
-Seamlessly_Login_Page seamlesslyLoginPage = new Seamlessly_Login_Page();
+    Seamlessly_Login_Page seamlesslyLoginPage = new Seamlessly_Login_Page();
 
     @Given("the user on the log in page")
     public void the_user_on_the_log_in_page() {
         Driver.getDriver().get(ConfigurationReader.getProperty("seamleasslyURL"));
     }
+
     @When("the user enter valid username")
     public void the_user_enter_valid_username() {
-   seamlesslyLoginPage.userName.sendKeys(ConfigurationReader.getProperty("seamlesslyUsername"));
+        seamlesslyLoginPage.userName.sendKeys(ConfigurationReader.getProperty("seamlesslyUsername"));
 
     }
+
     @When("the user enter valid password")
     public void the_user_enter_valid_password() {
-seamlesslyLoginPage.password.sendKeys(ConfigurationReader.getProperty("seamlesslyPassword"));
+        seamlesslyLoginPage.password.sendKeys(ConfigurationReader.getProperty("seamlesslyPassword"));
 
 
     }
+
     @When("the user clicks the log in button")
     public void the_user_clicks_the_log_in_button() {
-   seamlesslyLoginPage.loginButton.click();
+        seamlesslyLoginPage.loginButton.click();
 
     }
+
     @Then("the user should be able to log in")
     public void the_user_should_be_able_to_log_in() {
 
-        Assert.assertTrue(Driver.getDriver().getTitle().contains("dashboard"));
+        Assert.assertTrue(Driver.getDriver().getTitle().contains("seamlessly"));
+    }
+
+
+    @When("the user enter valid password {string}")
+    public void the_user_enter_valid_password_With_Param(String password) {
+        if (password.equals("empty")) {
+            seamlesslyLoginPage.password.sendKeys("");
+        } else {
+            seamlesslyLoginPage.password.sendKeys(password);
+        }
+    }
+
+    @When("the user enter valid username {string}")
+    public void the_user_enter_valid_username_With_Param(String username) {
+        if (username.equals("empty")) {
+            seamlesslyLoginPage.userName.sendKeys("");
+        } else {
+            seamlesslyLoginPage.userName.sendKeys(username);
+
+        }
     }
 }
--- src/test/resources/features/LoghinSeamlessly.feature
+++ src/test/resources/features/LoghinSeamlessly.feature
@@ -1,10 +1,41 @@
 Feature: Login Functionality tests
 
 
-  @validLogin
   Scenario: Login with valid credentials
     Given the user on the log in page
     When the user enter valid username
     And the user enter valid password
     And the user clicks the log in button
     Then the user should be able to log in
+
+
+  Scenario: Login with valid credentials
+    Given the user on the log in page
+    When the user enter valid username "Employee61"
+    And the user enter valid password "Employee123"
+    And the user clicks the log in button
+    Then the user should be able to log in
+
+
+  Scenario: Login with valid credentials
+    Given the user on the log in page
+    When the user enter valid username "Employee61"
+    And the user enter valid password "Employee123"
+    And the user clicks the log in button
+    Then the user should be able to log in
+
+  @validLoginWithParametres
+  Scenario Outline: Login with valid credentials
+    Given the user on the log in page
+    When the user enter valid username "<username>"
+    And the user enter valid password "<password>"
+    And the user clicks the log in button
+    Then the user should be able to log in
+
+    Examples:
+      | username      | password     |
+      | Employee61    | Employee123  |
+      | wrongUserName | Employee123  |
+      | Employee61    | WrongPasword |
+      | WrongUsername | WrongPasword |
+      | empty         | empty        |
